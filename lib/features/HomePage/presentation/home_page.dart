e48244730a75ecebae3636f78cf715e1871be461
Ayat Esaam <eayatessam1@gmail.com>
2026-03-13 10:20:23 +0200

    add CutsomAppBar in homePage

---
+++ lib/features/HomePage/presentation/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/components/custom_app_bar.dart';
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(60),
+          child: CustomAppBar()),
+      body:Column(
+
+      ));
+  }
+}
--- lib/features/home/data/repo/home_repo_implementation.dart
+++
@@ -1,3 +0,0 @@
-import 'package:news_app/features/home/data/repo/home_repo.dart';
-
-class HomeRepoImplementation  implements HomeRepo{}
