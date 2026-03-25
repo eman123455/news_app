2c2f086f2de4c9a927b9c33b1dbaba0de2e45983
Eman Kamel <kamele432@gmail.com>
2026-03-25 23:47:35 +0200

    create custom profile photo& profile state item

---
+++ lib/features/profile/presentation/widgets/custom_profile_photo.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:news_app/core/components/cache_image.dart';
+
+class CustomProfilePhoto extends StatelessWidget {
+  const CustomProfilePhoto({
+    super.key,
+    required this.imageUrl,
+    this.userName,
+    this.width,
+    this.height, this.fontsize,
+  });
+  final String imageUrl;
+  final String? userName;
+  final double? width;
+  final double? height;
+  final double? fontsize;
+  String getFirstLetter(String? name) {
+    if (name == null || name.isEmpty) return 'U';
+    return name[0].toUpperCase();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(100),
+      child: CacheImage(
+        imageUrl: imageUrl,
+        width: width,
+        height: height,
+        fontSize: fontsize,
+        userFristLetter: getFirstLetter(userName),
+      ),
+    );
+  }
+}
