4e53feb4bfb79ff8edebbdc2956e173d564cce88
Eman Kamel <kamele432@gmail.com>
2026-03-25 23:40:18 +0200

    create  profile cubit and post cubit

--- lib/features/profile/profile_business_logic/profile_cubit/profile_cubit.dart
+++ lib/features/profile/profile_business_logic/profile_cubit/profile_cubit.dart
@@ -1,8 +1,53 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:news_app/core/utils/service_locator.dart';
+import 'package:news_app/features/profile/data/models/profile_model.dart';
+import 'package:news_app/features/profile/data/repo/profile_repo_implementation.dart';
 
 part 'profile_state.dart';
 
 class ProfileCubit extends Cubit<ProfileState> {
   ProfileCubit() : super(ProfileInitial());
+  final repo = getIt.get<ProfileRepoImplementation>();
+  // test
+  final userId = '062b90f8-49cd-4911-8d3a-265924aa0597';
+  Future<void> getProfile() async {
+    emit(GetProfileLoading());
+    try {
+      final profile = await repo.getProfile(userId);
+      emit(GetProfileSuccess(profile: profile));
+    } catch (e) {
+      emit(GetProfileFailed(errMsg: e.toString()));
+    }
+  }
+
+  Future<void> updateProfile({
+    String? username,
+    String? fullName,
+    String? email,
+    String? phone,
+    String? bio,
+    String? website,
+    String? imageUrl,
+    String? country,
+    String? role,
+  }) async {
+    try {
+      await repo.updateProfile(
+        userId: userId,
+        username: username,
+        fullName: fullName,
+        email: email,
+        phone: phone,
+        bio: bio,
+        website: website,
+        imageUrl: imageUrl,
+        country: country,
+        role: role,
+      );
+      await getProfile();
+    } catch (e) {
+      emit(ProfileUpdateFailed(errMsg: e.toString()));
+    }
+  }
 }
--- lib/features/profile/profile_business_logic/profile_cubit/profile_state.dart
+++ lib/features/profile/profile_business_logic/profile_cubit/profile_state.dart
@@ -4,3 +4,25 @@ part of 'profile_cubit.dart';
 sealed class ProfileState {}
 
 final class ProfileInitial extends ProfileState {}
+
+final class GetProfileSuccess extends ProfileState {
+  final ProfileModel profile;
+
+  GetProfileSuccess({required this.profile});
+}
+
+final class GetProfileFailed extends ProfileState {
+  final String errMsg;
+
+  GetProfileFailed({required this.errMsg});
+}
+
+final class GetProfileLoading extends ProfileState {}
+
+final class ProfileUpdateSuccess extends ProfileState {}
+final class ProfileUpdateFailed extends ProfileState {
+  final String errMsg;
+
+  ProfileUpdateFailed({required this.errMsg});
+
+}
